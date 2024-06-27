f289beeb755bc518750992ae6a608157719d04c7
Felix Betz <Betz.Felix@web.de>
2024-06-27 23:56:27 +0200

    parse birthdays for tent leaders

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -259,9 +259,8 @@
 			}
 		});
 
-		beginZlDate.setDate(beginZlDate.getDate() - 3);
+		beginZlDate.setDate(beginZlDate.getDate() - 2);
 		endZlDate.setDate(beginZlDate.getDate() + 1);
-
 		pTentLeader.forEach((p) => {
 			const [year, month, day] = p.birthdate.split('-');
 			const birthDate = new Date(zlYear, +month - 1, +day); //(0 = January to 11 = December)
